a01d1c057c2c5eedb8d5bf7a88d2a9637f3f8969
Furkan Güneş <m_furkan64@hotmail.com>
2021-07-15 17:44:24 +0300

    Key char fails on turkish chars

--- gui.py
+++ gui.py
@@ -9,6 +9,8 @@ class Frame(tk.Frame):
         tk.Frame.__init__(master=master, cnf=cnf, **kwargs)
 
 GIT_LINK = "https://github.com/furkangunes/Synth"
+MAIN_NOTE_COLOR = "orange"
+SHARP_NOTE_COLOR = "black"
 
 class Gui(tk.Tk):
     def __init__(self, player: Player, *args, **kwargs):
@@ -17,11 +19,12 @@ class Gui(tk.Tk):
         self.player = player
         self.keyboard = NoteFactory.create_keyboard(key_count=20) # Might change key count
         self.key_dict = self.get_key_dict()
+        self.button_dict = dict.fromkeys(self.key_dict.keys())
 
         self.explanation = "Press keys to play sound"
 
         self.header = tk.Frame(master=self, width=200, height=100)
-        self.keyboard_frame = tk.Frame(master=self, width=200, height=100, bg="green")
+        self.keyboard_frame = tk.Frame(master=self, width=200, height=100)
         self.footer = tk.Frame(master=self, width=200, height=100, bg="blue")
 
         self.configure_frames()
@@ -29,9 +32,8 @@ class Gui(tk.Tk):
 
         self.set_bindings()
         self.protocol("WM_DELETE_WINDOW", self.on_close)
-        #self.header = tk.Frame(self, row=0, column=0, sticky="NSWE")
 
-        print("No action" + " " * 7, end="\r")
+        #print("No action" + " " * 7, end="\r")
 
     def configure_frames(self):
         self.title("Synthesizer")
@@ -43,17 +45,61 @@ class Gui(tk.Tk):
         self.update()
         self.minsize(self.winfo_width(), self.winfo_height())
 
-        tk.Label(master=self.header, text="Synthesizer", bg="red").grid(row=0)
-        tk.Label(master=self.header, text=self.explanation).grid(row=1)
+        tk.Label(master=self.header, text="Synthesizer", bg="red").grid(row=0, sticky="NSEW")
+        tk.Label(master=self.header, text=self.explanation).grid(row=1, sticky="NSEW")
 
         tk.Label(master=self.footer, text=GIT_LINK, bg="red").grid(row=0)
 
     def configure_keyboard_frame(self):
-        for i in range(8):
-            tk.Frame(master=self.keyboard_frame, width=50, height=100, bg="orange", highlightthickness=1).grid(row=0, column=i)
+        # for i in range(6):
+        #     tk.Button(master=self.keyboard_frame, width=5, height=20, bg=SHARP_NOTE_COLOR, highlightthickness=True).place(x=i*5 + 10, y=0)
 
-        for i in range(12):
-            tk.Frame(master=self.keyboard_frame, width=50, height=100, bg="orange", highlightthickness=1).grid(row=1, column=i)
+        button_width: int
+
+        for i, key in zip(range(12), "asdfghjklşi,"):
+            note_name = self.key_dict[key] + "\n" * 5 + key.upper()
+            button = tk.Button(
+                master=self.keyboard_frame,
+                width=10,
+                height=20,
+                text=note_name,
+                anchor="s",
+                foreground=SHARP_NOTE_COLOR,
+                bg=MAIN_NOTE_COLOR,
+                highlightthickness=True
+            )
+            button.grid(row=0, column=i)
+
+            self.button_dict[key] = button
+
+        self.update()    
+        button_width = button.winfo_width()
+
+        sharp_buttons = []
+        for key in "weryuıpğ":
+            button = tk.Button(
+                master=self.keyboard_frame,
+                width=5,
+                height=10,
+                text=self.key_dict[key] + "\n" * 5 + key.upper(),
+                anchor="s",
+                foreground="white",
+                bg=SHARP_NOTE_COLOR,
+                highlightthickness=True
+            )
+
+            sharp_buttons.append(button)
+            self.button_dict[key] = button
+
+        # Below is hard coded :(
+        sharp_buttons[0].place(x=button_width - 25)
+        sharp_buttons[1].place(x=button_width * 3 - 25)
+        sharp_buttons[2].place(x=button_width * 4 - 25)
+        sharp_buttons[3].place(x=button_width * 6 - 25)
+        sharp_buttons[4].place(x=button_width * 7 - 25)
+        sharp_buttons[5].place(x=button_width * 8 - 25)
+        sharp_buttons[6].place(x=button_width * 10 - 25)
+        sharp_buttons[7].place(x=button_width * 11 - 25)
 
     def get_key_dict(self, octave_number=4):
         return {
@@ -79,18 +125,26 @@ class Gui(tk.Tk):
             ",": "E" + str(octave_number + 2)
         }
 
-    def change_note(self, note_name):
-        print("Active Note:", note_name, end="\r")
+    def activate_note(self, note_name):
+        #print("Active Note:", note_name, end="\r")
         self.player.freq = self.keyboard[note_name].freq
 
     def on_press(self, key):
-        key_name = key.char
+        key_name = key.char.lower()
 
         if key_name in self.key_dict:
-            self.change_note(self.key_dict[key_name])
+            self.button_dict[key_name]["bg"] = "red"
+            self.activate_note(self.key_dict[key_name])
 
     def on_release(self, key):
-        print("No action" + " " * 7, end="\r")
+        #print("No action" + " " * 7, end="\r")
+
+        key_name = key.char#.lower()
+
+        print(key)
+        if key_name in self.button_dict:
+            self.button_dict[key_name]["bg"] = MAIN_NOTE_COLOR if key_name in "asdfghjklşi," else SHARP_NOTE_COLOR
+
         self.player.freq = 0
 
     def on_close(self):
